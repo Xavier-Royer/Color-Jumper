9cf9ee3896a52f7483d7bf805d5b171d62fa1465
Seventeen <90122630+7Teen17@users.noreply.github.com>
2025-08-17 15:22:13 -0700

    Change export presets and icon.svg

--- game_screen.gd
+++ game_screen.gd
@@ -138,7 +138,7 @@ func loadGame():
 		movingObjects.add_child(block)
 		block.position = Vector2(randi_range(30,screen_size.x - 35),randi_range(-600,screen_size.y * (2.0/3.0) - 100))
 		#block.connect("invalidBlock",spawnBlock)
-		block.setColor("RAINBOW")
+		block.setColor("RED")
 		block.connect("blockMissed",gameOver)
 		lastBlockSpawned = block
 		blocksSpawned+=1
