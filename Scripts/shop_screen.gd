85b266df447050b2a661c6cefdf5b5ea3144e0e4
7Teen17 <dominic.siino@gmail.com>
2025-11-02 21:26:01 -0800

    shop screen ui update

--- Scripts/shop_screen.gd
+++ Scripts/shop_screen.gd
@@ -9,21 +9,33 @@ func _on_selection_changed(item: ShopSlot, _index: int) -> void:
 			CollectibleDB.CURRENT["skin"] = collectible
 			$Player/ColorRect.texture = collectible.icon
 			if collectible.id not in CollectibleDB.OWNED:
-				$SkinBuySection.visible = true
+				$SkinBuySection/SkinCost.visible = true
+				$SkinBuySection/SkinBuy.visible = true
+				$SkinBuySection/EquippedLabel.visible = false
 			else:
-				$SkinBuySection.visible = false
+				$SkinBuySection/SkinCost.visible = false
+				$SkinBuySection/SkinBuy.visible = false
+				$SkinBuySection/EquippedLabel.visible = true
 		"trail":
 			CollectibleDB.CURRENT["trail"] = collectible
 			if collectible.id not in CollectibleDB.OWNED:
-				$TrailBuySection.visible = true
+				$TrailBuySection/SkinCost.visible = true
+				$TrailBuySection/SkinBuy.visible = true
+				$TrailBuySection/EquippedLabel.visible = false
 			else:
-				$TrailBuySection.visible = false
+				$TrailBuySection/SkinCost.visible = false
+				$TrailBuySection/SkinBuy.visible = false
+				$TrailBuySection/EquippedLabel.visible = true
 		"theme":
 			CollectibleDB.CURRENT["theme"] = collectible
 			if collectible.id not in CollectibleDB.OWNED:
-				$ThemeBuySection.visible = true
+				$ThemeBuySection/SkinCost.visible = true
+				$ThemeBuySection/SkinBuy.visible = true
+				$ThemeBuySection/EquippedLabel.visible = false
 			else:
-				$ThemeBuySection.visible = false
+				$ThemeBuySection/SkinCost.visible = false
+				$ThemeBuySection/SkinBuy.visible = false
+				$ThemeBuySection/EquippedLabel.visible = true
 	
 		
 
@@ -31,15 +43,15 @@ func _on_selection_changed(item: ShopSlot, _index: int) -> void:
 func _on_home_pressed() -> void:
 	if CollectibleDB.CURRENT["skin"].id not in CollectibleDB.OWNED:
 		CollectibleDB.CURRENT["skin"] = CollectibleDB.get_default_skin()
-		$Options/skin.set_default()
+		$TabContainer/Skins.set_default()
 		$Player/ColorRect.texture =CollectibleDB.get_default_skin().icon
 	if CollectibleDB.CURRENT["trail"].id not in CollectibleDB.OWNED:
 		CollectibleDB.CURRENT["trail"] = CollectibleDB.get_default_trail()
-		$Options/trail.set_default()
+		$TabContainer/Trails.set_default()
 		#set shop menu trail here
 	if CollectibleDB.CURRENT["trail"].id not in CollectibleDB.OWNED:
 		CollectibleDB.CURRENT["trail"] = CollectibleDB.get_default_theme()
-		$Options/theme.set_default()
+		$TabContainer/Themes.set_default()
 		#set shop menu theme here
 		
 	$"../GameScreen/Objects/Player/ColorRect".texture = CollectibleDB.CURRENT["skin"].icon
