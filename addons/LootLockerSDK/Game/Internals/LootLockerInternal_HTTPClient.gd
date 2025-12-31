c8e1fc39f251cf43f2e0536d6facf6edd754cce0
7Teen17 <dominic.siino@gmail.com>
2025-12-30 23:54:57 -0800

    Revert "possible fix: change url for lootlocker reqs"
    This reverts commit 03db74173831f346b2a6367bbe621fce2f7b2e03.

--- addons/LootLockerSDK/Game/Internals/LootLockerInternal_HTTPClient.gd
+++ addons/LootLockerSDK/Game/Internals/LootLockerInternal_HTTPClient.gd
@@ -43,22 +43,14 @@ static func logLootLockerRequest(endpoint, requestType, body, result : LL_HTTPRe
 
 func makeRequest(endpoint, requestType: HTTPClient.Method, body, additionalHeaders : Array[String] = []) -> LL_HTTPRequestResult:
 	var httpClient = HTTPClient.new()
-	var base_url: String = LootLockerSettings.GetUrl().strip_edges()
-
-	# base_url is like: "https://<domainKey>.api.lootlocker.com"
-	var use_tls := base_url.begins_with("https://")
-	var host := base_url.replace("https://", "").replace("http://", "")
-
-	var tls := TLSOptions.client() if use_tls else null
-	
-
+	var url = LootLockerSettings.GetUrl()
 	
 	if !httpClient:
 		var res = LL_HTTPRequestResult.new("{ \"message\": \"LootLocker could not create http client\"}", 0, false, -1)
 		logLootLockerRequest(endpoint, requestType, body, res)
 		return res
 	
-	var err := httpClient.connect_to_host(host, -1, tls)
+	var err = httpClient.connect_to_host(url)
 	
 	if err != OK:
 		var res = LL_HTTPRequestResult.new("{ \"message\": \"Could not connect to LootLocker, error code was "+str(err)+"\"}", 0, false, -1)
