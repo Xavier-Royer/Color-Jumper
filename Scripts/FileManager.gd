0bf2a6b4cb0e30e751592f67ae8c5006e2617618
7Teen17 <dominic.siino@gmail.com>
2026-01-01 23:17:19 -0800

    fix: update encryption

--- Scripts/FileManager.gd
+++ Scripts/FileManager.gd
@@ -5,7 +5,7 @@ var tutorial = true
 var sound_enabled = true
 var sfx_enabled = true
 var coins = 0
-var flpswd = "SUPERSECRET123ABC909090"
+var flpswd = "ULTRASECRET90210"
 
 
 var owned = [
@@ -39,16 +39,16 @@ func setHighScore(score,index):
 	
 
 func saveSettings():
-	var file = FileAccess.open_encrypted_with_pass("user://settings.dat", FileAccess.WRITE, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://settings2.dat", FileAccess.WRITE, flpswd)
 	file.store_var(difficulty)
 	file.store_var(sound_enabled)
 	file.store_var(sfx_enabled)
 	file.close()
 	
 func loadSettings():
-	if !FileAccess.file_exists("user://settings.dat"):
+	if !FileAccess.file_exists("user://settings2.dat"):
 		return
-	var file = FileAccess.open_encrypted_with_pass("user://settings.dat", FileAccess.READ, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://settings2.dat", FileAccess.READ, flpswd)
 	difficulty = file.get_var()
 	sound_enabled = file.get_var()
 	sfx_enabled = file.get_var()
@@ -58,31 +58,31 @@ func loadSettings():
 
 
 func saveHighScore():
-	var file = FileAccess.open_encrypted_with_pass("user://highScore.dat", FileAccess.WRITE, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://highScore2.dat", FileAccess.WRITE, flpswd)
 	file.store_var(highScore)
 	file.close()
 	
 func loadHighScore():
-	var file = FileAccess.open_encrypted_with_pass("user://highScore.dat", FileAccess.READ, flpswd)
-	if FileAccess.file_exists("user://highScore.dat"):
+	var file = FileAccess.open_encrypted_with_pass("user://highScore2.dat", FileAccess.READ, flpswd)
+	if FileAccess.file_exists("user://highScore2.dat"):
 		highScore = file.get_var()
 		file.close()
 
 
 func saveTutorial():
-	var file = FileAccess.open_encrypted_with_pass("user://tutorial.dat", FileAccess.WRITE, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://tutorial2.dat", FileAccess.WRITE, flpswd)
 	file.store_var(false)
 	file.close()
 
 func loadTutorial():
-	var file = FileAccess.open_encrypted_with_pass("user://tutorial.dat", FileAccess.READ, flpswd)
-	if FileAccess.file_exists("user://tutorial.dat"):
+	var file = FileAccess.open_encrypted_with_pass("user://tutorial2.dat", FileAccess.READ, flpswd)
+	if FileAccess.file_exists("user://tutorial2.dat"):
 		tutorial = file.get_var()
 		file.close()
 
 func loadCoins():
-	var file = FileAccess.open_encrypted_with_pass("user://coins.dat", FileAccess.READ, flpswd)
-	if FileAccess.file_exists("user://coins.dat"):
+	var file = FileAccess.open_encrypted_with_pass("user://coins2.dat", FileAccess.READ, flpswd)
+	if FileAccess.file_exists("user://coins2.dat"):
 		coins = file.get_var()
 		if coins == null:
 			coins = 0
@@ -94,32 +94,32 @@ func loadCoins():
 
 
 func saveCoins():
-	var file = FileAccess.open_encrypted_with_pass("user://coins.dat", FileAccess.WRITE, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://coins2.dat", FileAccess.WRITE, flpswd)
 	file.store_var(coins)
 	file.close()
 
 
 func loadOwned():
-	var file = FileAccess.open_encrypted_with_pass("user://owned.dat", FileAccess.READ, flpswd)
-	if FileAccess.file_exists("user://owned.dat"):
+	var file = FileAccess.open_encrypted_with_pass("user://owned2.dat", FileAccess.READ, flpswd)
+	if FileAccess.file_exists("user://owned2.dat"):
 		owned = file.get_var()
 		file.close()
 
 
 func saveOwned():
-	var file = FileAccess.open_encrypted_with_pass("user://owned.dat", FileAccess.WRITE, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://owned2.dat", FileAccess.WRITE, flpswd)
 	file.store_var(owned)
 	file.close()
 
 func saveCurrentCollectibles():
-	var file = FileAccess.open_encrypted_with_pass("user://current.dat", FileAccess.WRITE, flpswd)
+	var file = FileAccess.open_encrypted_with_pass("user://current2.dat", FileAccess.WRITE, flpswd)
 	file.store_var(CollectibleDB.CURRENT)
 	file.close()
 	
 
 func loadCurrentCollectibles():
-	var file = FileAccess.open_encrypted_with_pass("user://current.dat", FileAccess.READ, flpswd)
-	if FileAccess.file_exists("user://current.dat"):
+	var file = FileAccess.open_encrypted_with_pass("user://current2.dat", FileAccess.READ, flpswd)
+	if FileAccess.file_exists("user://current2.dat"):
 		var data = file.get_var()
 		CollectibleDB.CURRENT = data
 		file.close()
